904fb25c36ce322e9020860b44b4e158d1c4931a
Aaron Lohner <aaron.lohner@gmail.com>
2020-07-15 20:30:35 -0400

    can now read from HistReport and write to AppData, but must add feature to add new app names

--- update.py
+++ update.py
@@ -5,15 +5,19 @@ from re import search, findall
 from math import floor
 from datetime import date, time, timedelta
 
-path = "screentime_tracker/HistoryReport.xlsx"
+hist_path = "screentime_tracker/HistoryReport.xlsx"
+data_path = "screentime_tracker/AppData.xlsx"
 # wb is short for workbook
-wb = load_workbook(path)
+wbh = load_workbook(hist_path)
+wbd = load_workbook(data_path)
+wbd.active = wbd.get_sheet_by_name("Data")
+wsd = wbd.active
 
 def sheetDates(sheet_dates):
     # creates a tupled list of all sheets generated from Samsung app in the form (date, name), where
     # date comes from their sheet names and name is the sheet name
     
-    sheet_names = (name for name in wb.sheetnames)
+    sheet_names = (name for name in wbh.sheetnames)
     for name in sheet_names:
         # having trouble using backreferences, just repeating the same group instead
         if search("^\d{2}-\d{2}-\d{4}_\d{2}-\d{2}-\d{2}$", name):
@@ -25,17 +29,14 @@ def sheetDates(sheet_dates):
 
 def defName(defined_name_in_wb):
     # returns a tuple containing all the cell values present in the Data sheet in a defined name
-    # sets wb.active to the Data sheet
 
-    wb.active = wb.get_sheet_by_name("Data")
-    ws = wb.active
-    defined = wb.defined_names[defined_name_in_wb]
+    defined = wbd.defined_names[defined_name_in_wb]
     # dests is a tuple generator of (worksheet title, cell range)
     dests = defined.destinations
     _, coord = next(dests)
     # Strangely enough, ws[cell_range_str] returns a single tuple with all the cells representing app names
     # inside the first inner tuple
-    return ws[coord][0]
+    return wsd[coord][0]
 
 def convertToTime(time_str):
     time_str = str(time_str)
@@ -88,16 +89,15 @@ def main():
     # WITH DATA THAT HAS ALREADY BEEN ADDED
     
     # assuming order of time_info cells is Season, DOTW, Date but not assuming their coordinates in the sheet
+    
     time_info_cells = defName('time_info')
     # get a tuple with all the app cells in the Data sheet
     app_names_in_data = defName('app_names')
     # make a list for app values
     app_values_in_data = [app.value for app in app_names_in_data]
 
-    wb.active = wb.get_sheet_by_name("Data")
-    ws = wb.active
     # first empty row that will be populated in Data sheet
-    top_row = ws.max_row + 1
+    top_row = wsd.max_row + 1
 
     season = "Summer"
     # how can I take in input?
@@ -107,59 +107,56 @@ def main():
     days = ["Sa", "Su", "M", "Tu", "W", "Th", "F"]
     for sheet in sheet_dates:
         # start by adding the time info
-        wb.active = wb.get_sheet_by_name("Data")
-        ws = wb.active
         for cell in time_info_cells:
             if cell.value == "Season":
                 for i in range(0,7):
-                    ws.cell(column=cell.column, row=top_row+i, value=season)
+                    wsd.cell(column=cell.column, row=top_row+i, value=season)
             if cell.value == "DOTW":
                 for i, day in zip(range(0,7), days):
-                    ws.cell(column=cell.column, row=top_row+i, value=day)
+                    wsd.cell(column=cell.column, row=top_row+i, value=day)
             if cell.value == "Date":
                 for i in range(0,7):
-                    ws.cell(column=cell.column, row=top_row+i, value=sheet[0]-timedelta(days=6-i))
+                    wsd.cell(column=cell.column, row=top_row+i, value=sheet[0]-timedelta(days=6-i))
 
         # iterate over sheets from the least recent sheet the most
-        wb.active = wb.get_sheet_by_name(sheet[1])
-        ws = wb.active
+        wbh.active = wbh.get_sheet_by_name(sheet[1])
+        wsh = wbh.active
         # max_row row value is a total row, so skip it
 
-
         #CHANGE THIS BACK TO VALUES ONLY
-        get_cols = ws.iter_cols(min_row=0, max_row=ws.max_row-1)#, values_only=True)
+        get_cols = wsh.iter_cols(min_row=0, max_row=wsh.max_row-1, values_only=True)
         # dealing with column of apps. Skip over first cell, which is blank
         app_names_in_sheet = next(get_cols)[1:]
+
         # generate the app times, app by app
-        app_times = ws.iter_rows(min_row=2, max_row=ws.max_row-1, min_col=2, max_col=8, values_only=True)
+        app_times = wsh.iter_rows(min_row=2, max_row=wsh.max_row-1, min_col=2, max_col=8, values_only=True)
         for app in app_names_in_sheet:
             #*** deal with case of new apps later*** Will have to edit app_names defined name
-            if app.value in app_values_in_data:
-                # go to sheet to get values for time spent, getting all 7 values for a given app per iteration
-                #wb.active = wb.get_sheet_by_name(sheet[1])
-                #ws = wb.active
+            if app in app_values_in_data:
+                # get values for time spent, getting all 7 values for a given app per iteration
                 # for duplicate named case
                 #if app_values_in_data.count(app.value) > 1:
 
-                # go back to Data sheet to write stuff in
-                wb.active = wb.get_sheet_by_name("Data")
-                ws = wb.active
                 for i, time in zip(range(0,7), next(app_times)):
                     time = convertToTime(time)
                     # if the top cell of the column to which we are about to write is empty, go ahead
-                    if ws.cell(column=app_names_in_data[app_values_in_data.index(app.value)].column, row=top_row+i).value is None:
-                        # write in the time data under the column header corresponding to the app name, over 7 rows
-                        ws.cell(column=app_names_in_data[app_values_in_data.index(app.value)].column, row=top_row+i, value=time)
+                    if wsd.cell(column=app_names_in_data[app_values_in_data.index(app)].column, \
+                            row=top_row+i).value is None:
+                        # write in the time data under the column header corresponding to the app name,
+                        # over 7 rows
+                        wsd.cell(column=app_names_in_data[app_values_in_data.index(app)].column, \
+                            row=top_row+i, value=time)
                     # the cell will not be empty if the app name is a duplicate. In this case, find it and
                     # write data over there instead
                     else:
-                        if app_values_in_data.count(app.value) > 1:
-                            # insert data at next occurrence of app name; start searching through app list starting
-                            # one position after first occurrence
-                            ws.cell(column=app_names_in_data[app_values_in_data.index(app.value, app_values_in_data.index(app.value)+1)].column, row=top_row+i, value=time)
+                        if app_values_in_data.count(app) > 1:
+                            # insert data at next occurrence of app name; start searching through app list
+                            # starting one position after first occurrence
+                            wsd.cell(column=app_names_in_data[app_values_in_data.index(app, \
+                                app_values_in_data.index(app)+1)].column, row=top_row+i, value=time)
                         # else: ***case of a new app***
 
-    wb.save(path)
+    wbd.save(data_path)
 
     '''
     TODO:
