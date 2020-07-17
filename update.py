87457cb9b7c0ca1f784e599c1928f03ed7e988b9
Aaron Lohner <aaron.lohner@gmail.com>
2020-07-16 21:24:22 -0400

    add back feature to remove from sheet_dates list, change days from list to dict, try to add feature to modify app_names defined name when adding new apps, not done

--- update.py
+++ update.py
@@ -22,8 +22,8 @@ def sheetDates(sheet_dates):
         # having trouble using backreferences, just repeating the same group instead
         if search("^\d{2}-\d{2}-\d{4}_\d{2}-\d{2}-\d{2}$", name):
             # make sure all sheets have been generated on Fridays, if not throw an Exception
-            if date(int(name[6:10]), int(name[3:5]), int(name[0:2])).weekday() != 4:
-                raise Exception("Not all sheets were generated on a Friday")
+            #if date(int(name[6:10]), int(name[3:5]), int(name[0:2])).weekday() != 4:
+            #    raise Exception("Not all sheets were generated on a Friday")
             # append date object from sheet name with the sheet name. Date objects are in the form y,m,d
             sheet_dates.append((date(int(name[6:10]), int(name[3:5]), int(name[0:2])), name))
 
@@ -34,7 +34,7 @@ def defName(defined_name_in_wb):
     # dests is a tuple generator of (worksheet title, cell range)
     dests = defined.destinations
     _, coord = next(dests)
-    # Strangely enough, ws[cell_range_str] returns a single tuple with all the cells representing app names
+    # Strangely enough, wsd[coord] returns a single tuple with all the cells representing app names
     # inside the first inner tuple
     return wsd[coord][0]
 
@@ -66,31 +66,24 @@ def main():
     # sort dates from the earliest to the most recent
     sheet_dates.sort(key=lambda tup: tup[1])
 
-    '''
     # get the most recent date recorded in Data
-
-    # the following string represents the range of cells representing the most recent date in the Data sheet
-    cell_range_str = "B" + str(ws.max_row) + ":" + "D" + str(ws.max_row)
-    # convert to a date. Strangely enough, ws[cell_range_str] returns a single tuple with a triple tuple
-    # inside that has the 3 cell values corresponding to the date
-    y, m, d = ws[cell_range_str][0]
-    most_recent_data_date = date(y.internal_value, m.internal_value, d.internal_value)
-
-    get_later_date = (date[0] for date in sheet_dates)
-    i = -1
-    while next(get_later_date) > most_recent_data_date:
-        i = i+1
-    # delete sheets from sheet_dates that are from dates prior to the most recent date in Data, i.e.
-    # their values have already been added
-    if i >= 0:
-        del sheet_dates[i:]
-    '''
-    # MOST OF THE ABOVE CODE CAN BE REMOVED IF I JUST ADD A FEATURE AT THE END OF THIS CODE TO REMOVE SHEETS
-    # WITH DATA THAT HAS ALREADY BEEN ADDED
-    
-    # assuming order of time_info cells is Season, DOTW, Date but not assuming their coordinates in the sheet
-    
+    # assumes order of time_info cells is Season, DOTW, Date but does not assume their coordinates in the sheet
     time_info_cells = defName('time_info')
+    most_recent_data_date = wsd.cell(column=time_info_cells[2].column, row=wsd.max_row).value
+    # only continue if Data sheet was originally populated with some date
+    if most_recent_data_date != "Date":
+        # convert from datetime.datetime to datetime.date
+        most_recent_data_date = date(most_recent_data_date.year, most_recent_data_date.month, most_recent_data_date.day)
+        get_later_date = (date[0] for date in sheet_dates)
+        i = 0
+        while i < len(sheet_dates) and next(get_later_date) <= most_recent_data_date:
+            i = i+1
+            print(i)
+        # delete sheets from sheet_dates that are from dates prior to the most recent date in Data, i.e.
+        # their values have already been added
+        if i > 0:
+            del sheet_dates[:i]
+    
     # get a tuple with all the app cells in the Data sheet
     app_names_in_data = defName('app_names')
     # make a list for app values
@@ -104,16 +97,23 @@ def main():
     #season = input("What season is it? If you do not enter one, the last season in the table will be used.")
     #if not season:
     #    season = 
-    days = ["Sa", "Su", "M", "Tu", "W", "Th", "F"]
+    days = {0:"M", 1:"Tu", 2:"W", 3:"Th", 4:"F", 5:"Sa", 6:"Su"}
+    new_app_counter = 0
     for sheet in sheet_dates:
+        counter_at_start = new_app_counter
         # start by adding the time info
         for cell in time_info_cells:
             if cell.value == "Season":
                 for i in range(0,7):
                     wsd.cell(column=cell.column, row=top_row+i, value=season)
             if cell.value == "DOTW":
-                for i, day in zip(range(0,7), days):
-                    wsd.cell(column=cell.column, row=top_row+i, value=day)
+                for i in range(0,7):
+                    date_minus_i = sheet[0] - timedelta(days=6-i)
+                    # the value is found by converting the required date (date_minus_i) to a date object,
+                    # then using its weekday() attribute to query the days dictionary to return the desired
+                    # string abbreviation for the day
+                    wsd.cell(column=cell.column, row=top_row+i, value=days[date(date_minus_i.year, \
+                        date_minus_i.month, date_minus_i.day).weekday()])
             if cell.value == "Date":
                 for i in range(0,7):
                     wsd.cell(column=cell.column, row=top_row+i, value=sheet[0]-timedelta(days=6-i))
@@ -123,7 +123,6 @@ def main():
         wsh = wbh.active
         # max_row row value is a total row, so skip it
 
-        #CHANGE THIS BACK TO VALUES ONLY
         get_cols = wsh.iter_cols(min_row=0, max_row=wsh.max_row-1, values_only=True)
         # dealing with column of apps. Skip over first cell, which is blank
         app_names_in_sheet = next(get_cols)[1:]
@@ -134,9 +133,6 @@ def main():
             #*** deal with case of new apps later*** Will have to edit app_names defined name
             if app in app_values_in_data:
                 # get values for time spent, getting all 7 values for a given app per iteration
-                # for duplicate named case
-                #if app_values_in_data.count(app.value) > 1:
-
                 for i, time in zip(range(0,7), next(app_times)):
                     time = convertToTime(time)
                     # if the top cell of the column to which we are about to write is empty, go ahead
@@ -155,6 +151,31 @@ def main():
                             wsd.cell(column=app_names_in_data[app_values_in_data.index(app, \
                                 app_values_in_data.index(app)+1)].column, row=top_row+i, value=time)
                         # else: ***case of a new app***
+            # if the app was only downloaded this past week and was not in the data sheet previously
+            else:
+                # write in the app name as a column header
+                wsd.cell(column=wsd.max_column+1, row=top_row, value=app)
+                for i, time in zip(range(0,7), next(app_times)):
+                    time = convertToTime(time)
+                    # max_column is now column with new app name
+                    wsd.cell(column=wsd.max_column, row=top_row+i, value=time)
+                new_app_counter += 1
+        # lengthen app_names defined name range to include new apps from sheet, if new apps were added
+        if new_app_counter + counter_at_start != new_app_counter:
+            defined = wbd.defined_names['app_names']
+            # dests is a tuple generator of (worksheet title, cell range)
+            dests = defined.destinations
+            _, coord = next(dests)
+            first_cell = str(coord[0:3])
+            last_cell = str(wsd.cell(column=wbd.max_col, row=2).coordinate)
+            wbd.defined_names.delete('app_names')
+            # replace app_names with larger range including new apps for next iteration
+            new_range = wbd.defined_name.DefinedName('app_names', attr_text='Sheet!' + first_cell + ':'/
+                                 + last_cell)
+            wb.defined_names.append(new_range)
+
+
+
 
     wbd.save(data_path)
 
@@ -162,9 +183,7 @@ def main():
     TODO:
     -add data from app-generated sheets to Data table column by column, adding new columns when necessary
     -format a Total column to capture total time from all app columns
-    -delete sheets with added data
-
-    #remove sheets from sheet_dates
+    -add 'season' input option, asking if same season applies throughout and option to use past season
     '''
 if __name__ == "__main__":
     main()
